3d4cb0540748ee7ba11ae50c2e1f583854bff762
Yung-Jer <wongyungjer29@gmail.com>
2022-05-21 09:36:22 +0800

    update second solution of coin change

--- Leetcode/CoinChange.py
+++ Leetcode/CoinChange.py
@@ -1,3 +1,4 @@
+# Top Down DP (Memoization)
 class Solution(object):
     def coinChange(self, coins, amount):
         """
@@ -21,3 +22,21 @@ class Solution(object):
 
         ans = change(amount)
         return ans if ans < 1000000000 else -1
+
+# Bottom Up DP (DP Table)
+# Faster and less memory space
+class Solution2(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        dp = [100000000] * (amount + 1)
+        dp[0] = 0
+        
+        for coin in coins:
+            for i in range(coin, amount+1):
+                dp[i] = min(dp[i], dp[i-coin]+1)
+                
+        return dp[amount] if dp[amount] < 100000000 else -1
