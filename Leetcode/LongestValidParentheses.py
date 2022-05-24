1050e30dfcff7cefee634dcdbb5162c681ba2b42
Yung-Jer <wongyungjer29@gmail.com>
2022-05-24 10:10:51 +0800

    daily leetcode challenge

---
+++ Leetcode/LongestValidParentheses.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if len(s) == 0:
+            return 0
+        
+        stack = [-1]
+        maxlength = 0
+        for i in range(len(s)):
+            if s[i] == "(":
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                maxlength = max(maxlength, i - stack[-1])
+                
+        return maxlength
