5cd0c3d8b70488d41fd82282e53ec1a633aa8ff2
Yung-Jer <wongyungjer29@gmail.com>
2022-05-27 15:52:18 +0800

    update new one-liner solution

--- Leetcode/NumberOfStepsToReduceANumberToZero.py
+++ Leetcode/NumberOfStepsToReduceANumberToZero.py
@@ -13,3 +13,15 @@ class Solution(object):
             num = num >> 1
             count += 1
         return count - 1 # subtract away when num == 1, we add count by 1 after doing right shift
+
+class Solution2(object):
+    def numberOfSteps(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        binary_str = bin(num)
+         # binary string starts with 0b, so minus 2
+         # when num == 1, we just subtract by 1 and bitshift to the right by 1 is not needed, so minus 1 here
+         # In total minus 3
+        return len(binary_str) + binary_str.count('1') - 3
