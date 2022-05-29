3ae5108aa428a0367e48159bed6dd1eb10f545c7
Yung-Jer <wongyungjer29@gmail.com>
2022-05-29 23:21:50 +0800

    updated documentation

--- Leetcode/StepsToMakeArrayNon-decreasing.py
+++ Leetcode/StepsToMakeArrayNon-decreasing.py
@@ -1,3 +1,5 @@
+# Idea is to store [index, how many steps to remove all elements smaller than current element on the right]
+
 class Solution:
     def totalSteps(self, nums: list[int]) -> int:
         stack = []
@@ -6,7 +8,8 @@ class Solution:
             cur = 0
             while stack and nums[stack[-1][0]] < nums[i]:
                 idx, count = stack.pop()
-                cur = max(cur+1, count)
+                # Since next element is also smaller than current element, we take max(1, next element's count)
+                cur = max(cur+1, count) 
             ans = max(ans, cur)
             stack.append([i, cur])
         return ans
