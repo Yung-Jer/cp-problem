9aa04b82423ccfc011711d0a62e426f3e820d225
Yung-Jer <wongyungjer29@gmail.com>
2022-06-01 14:23:35 +0800

    random practice 1

---
+++ Leetcode/FinalPricesWithASpecialDiscountInAShop.py
@@ -0,0 +1,9 @@
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        res = prices[:]
+        for i in range(len(prices)-1,-1,-1):
+            for j in range(i+1, len(prices)):
+                if prices[j] <= prices[i]:
+                    res[i] -= prices[j]
+                    break
+        return res
