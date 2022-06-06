22dbca1b93ccadc0fb2bcdbb3087b7e051d9c322
Yung-Jer <wongyungjer29@gmail.com>
2022-06-06 09:37:42 +0800

    leetcode daily challenge

---
+++ Leetcode/IntersectionOfTwoLinkedLists.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Optional:
+    def __init__(self, node):
+        self.node = node
+
+# Two running pointers pointing to two heads of the linked lists
+# When the pointers pointing to None (i.e. end of the linked list), switch to head of another linked list
+# This will make two pointers eventually meet at a common node because they will run the same steps
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        first = headA
+        second = headB
+        
+        while first != second:
+            first = first.next if first != None else headB
+            second = second.next if second != None else headA
+            
+        return first
