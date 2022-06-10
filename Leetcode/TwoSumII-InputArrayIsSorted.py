8a7dad9007a64f9a6ad02cd0e867553e9e1dda7f
Yung-Jer <wongyungjer29@gmail.com>
2022-06-10 12:57:36 +0800

    updated new solution

--- Leetcode/TwoSumII-InputArrayIsSorted.py
+++ Leetcode/TwoSumII-InputArrayIsSorted.py
@@ -11,3 +11,17 @@ class Solution:
             
             if idx < n and a + numbers[idx] == target:
                 return [i+1, idx+1]
+
+class Solution2:
+    def twoSum(self, numbers: list[int], target: int) -> list[int]:
+        l = 0
+        r = len(numbers) - 1
+        
+        while l < r:
+            total = numbers[l] + numbers[r]
+            if total == target:
+                return [l+1, r+1]
+            elif total < target:
+                l += 1
+            else:
+                r -= 1
