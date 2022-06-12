d5592dcb2e78e411539a8fcaa0a2a8f1f6c0e7a7
Yung-Jer <wongyungjer29@gmail.com>
2022-06-13 00:53:11 +0800

    update new solution

--- Leetcode/2302. CountSubarraysWithScoreLessThanK.py
+++ Leetcode/2302. CountSubarraysWithScoreLessThanK.py
@@ -1,3 +1,5 @@
+# Solution 1: Sliding Window
+
 class Solution:
     def countSubarrays(self, nums: list[int], k: int) -> int:
         ans = 0
@@ -25,4 +27,30 @@ class Solution:
 
 # Credit to Leetcode user: https://leetcode.com/Bakerston/
 # Refer from: https://leetcode.com/problems/count-subarrays-with-score-less-than-k/discuss/2138959/Python-Explanation-with-pictures-sliding-window
+
+# Solution 2: Prefix Sum + Binary Search
+
+import itertools
+
+class Solution2:
+    def countSubarrays(self, nums: list[int], k: int) -> int:
+        acc = list(itertools.accumulate(nums, initial = 0))
+        n = len(nums)
+        res = 0
+        
+        def bisect(acc, idx):
+            left = idx
+            right = n
+            while left < right:
+                m = (left + right + 1) // 2
+                if (acc[m]-acc[idx]) * (m-idx) >= k:
+                    right = m-1
+                else:
+                    left = m
+            return left
+        
+        for i in range(n):
+            right = bisect(acc, i)
+            res += right - i
+        return res
             
