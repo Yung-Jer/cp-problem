05dbd3de0d1c0fa7ef76d8aef15ab329f6fd1d79
Yung-Jer <wongyungjer29@gmail.com>
2022-07-09 12:48:43 +0800

    updated new dp solution

--- Leetcode/1696. JumpGameVI.py
+++ Leetcode/1696. JumpGameVI.py
@@ -1,4 +1,5 @@
 import heapq
+import collections
 
 class Solution:
     def maxResult(self, nums: list[int], k: int) -> int:
@@ -16,3 +17,21 @@ class Solution:
             score = nums[i] + -1 * maxscore
             heapq.heappush(heap, (-score, i))
         return score
+
+# Method 2: DP with sliding window to find max dp value within range using deque
+
+class Solution2:
+    def maxResult(self, nums: list[int], k: int) -> int:
+        n = len(nums)
+        dp = [-10000000000] * n
+        dp[0] = nums[0]
+        
+        q = collections.deque()
+        for i in range(1, n):
+            while q and dp[i-1] >= dp[q[-1]]:
+                q.pop()
+            q.append(i-1)
+            while q[0] < i-k:
+                q.popleft()
+            dp[i] = dp[q[0]] + nums[i]
+        return dp[-1]
