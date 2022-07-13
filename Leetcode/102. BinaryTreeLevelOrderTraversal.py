441dafc169a588c65032126ee3b30d8c60fa591b
Yung-Jer <wongyungjer29@gmail.com>
2022-07-13 12:09:12 +0800

    daily leetcode challenge

---
+++ Leetcode/102. BinaryTreeLevelOrderTraversal.py
@@ -0,0 +1,30 @@
+import collections
+# Definition for a binary tree node.
+class Optional:
+    def __init__(self, val):
+        self.val = val
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> list[list[int]]:
+        if not root:
+            return []
+        q = collections.deque()
+        res = []
+        q.append(root)
+        
+        while q:
+            temp = []
+            for i in range(len(q)):
+                node = q.popleft()
+                temp.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            res.append(temp)
+            
+        return res
