5d61a60ea2e4d2212bc14455add6d3c3d64a9afb
Yung-Jer <wongyungjer29@gmail.com>
2022-10-23 10:37:21 +0800

    daily leetcode challenge

---
+++ Leetcode/645. SetMismatch.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findErrorNums(self, nums: list[int]) -> list[int]:
+        _seen = set()
+        temp = 0
+        for i, num in enumerate(nums):
+            if num in _seen:
+                rep = num
+            else:
+                temp ^= num
+            temp ^= (i+1)
+            _seen.add(num)
+        
+        return [rep, temp]
