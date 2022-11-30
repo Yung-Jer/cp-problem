21729a9d9c7d7cc8fba66b1a4a0a453ef54e5424
Yung-Jer <wongyungjer29@gmail.com>
2022-11-30 11:23:39 +0800

    daily leetcode challenge

---
+++ Leetcode/1207. UniqueNumberOfOccurrences.py
@@ -0,0 +1,5 @@
+from collections import Counter
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        cnt = Counter(arr)
+        return len(cnt.values()) == len(set(cnt.values()))
